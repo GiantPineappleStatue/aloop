6587acb7ed614ab49f7a06605496b3add244f97a
Jerry Wu <jerrywu.hr@gmail.com>
2023-09-20 19:24:39 -0700

    ch1

---
+++ retrival_test.py
@@ -0,0 +1,42 @@
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import DeepLake
+from langchain.agents import initialize_agent, Tool
+from langchain.agents import AgentType
+from langchain.llms import OpenAI
+from langchain.chains import RetrievalQA
+from dotenv import load_dotenv
+load_dotenv()
+
+
+llm = OpenAI(model="text-davinci-003", temperature=0)
+embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+
+my_activeloop_org_id = "giantpineapplestatue"
+my_activeloop_dataset_name = "langchain_course_from_zero_to_hero"
+dataset_path = f"hub://{my_activeloop_org_id}/{my_activeloop_dataset_name}"
+db = DeepLake(dataset_path=dataset_path, embedding_function=embeddings)
+
+
+retrieval_qa = RetrievalQA.from_chain_type(
+	llm=llm,
+	chain_type="stuff",
+	retriever=db.as_retriever()
+)
+
+tools = [
+    Tool(
+        name="Retrieval QA System",
+        func=retrieval_qa.run,
+        description="Useful for answering questions."
+    ),
+]
+
+agent = initialize_agent(
+	tools,
+	llm,
+	agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+	verbose=True
+)
+
+response = agent.run("When was Napoleone born?")
+print(response)
---
+++ router.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+
+def run_script(script_path):
+
+    subprocess.run(['python', script_path])
+
+if __name__ == '__main__':
+    subfolders = [f.path for f in os.scandir() if f.is_dir()]
+    scripts = [os.path.join(subfolder, 'script.py') for subfolder in subfolders]
+
+    for i, script in enumerate(scripts, start=1):
+        print(f"{i}. {os.path.basename(os.path.dirname(script))}")  # Display folder name
+
+    try:
+        choice = int(input("Which script do you want to run? (Enter the corresponding number): "))
+
+        if 0 < choice <= len(scripts):
+            run_script(scripts[choice-1])
+        else:
+            print("Invalid choice!")
+    except ValueError:
+        print("Please enter a valid number!")
